86dc00237f8ad8f1844ed7215c2328a1016dea34
Mikhail Kuzmin <mkuzmin231@gmail.com>
2022-06-16 23:19:54 +0700

    Improve Package.swift definition

--- App/Modules/Package.swift
+++ App/Modules/Package.swift
@@ -2,267 +2,186 @@
 
 import PackageDescription
 
-let content: [(Product, [Target])] = [
-    (
-        .library(name: "AppAssembler", targets: ["AppAssembler"]),
-        [
-            .target(
-                name: "AppAssembler",
-                dependencies: [
-                    "SharedInterfaces",
-                    "CatalogClient",
-                    "CatalogServer",
-                    "CatalogSource",
-                    "IPAddressProvider",
-                    "Database",
-                    "ToolKit",
-                    "Logger",
-                    // Features
-                    "RootFeature",
-                    "CatalogFeature",
-                    "AddItemFeature",
-                    "RemoteFeature",
-                    // External dependencies
-                    .product(name: "Swinject", package: "Swinject"),
-                ],
-                path: "Modules/AppAssembler/Sources"
-            )
-        ]
-    ),
-    (
-        .library(name: "ToolKit", targets: ["ToolKit"]),
-        [
-            .target(
-                name: "ToolKit",
-                path: "Modules/ToolKit/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "SharedInterfaces", targets: ["SharedInterfaces"]),
-        [
-            .target(
-                name: "SharedInterfaces",
-                dependencies: [
-                    "ToolKit",
-                    "Models",
-                    .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
-                ],
-                path: "Modules/SharedInterfaces/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "Models", targets: ["Models"]),
-        [
-            .target(
-                name: "Models",
-                path: "Modules/Models/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "Database", targets: ["Database"]),
-        [
-            .target(
-                name: "Database",
-                dependencies: [
-                    "ToolKit",
-                    "Swinject"
-                ],
-                path: "Modules/Database/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "Contracts", targets: ["Contracts"]),
-        [
-            .target(
-                name: "Contracts",
-                dependencies: [
-                    .product(name: "GRPC", package: "grpc-swift")
-                ],
-                path: "Modules/Contracts/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "IPAddressProvider", targets: ["IPAddressProvider"]),
-        [
-            .target(
-                name: "IPAddressProvider",
-                dependencies: ["Swinject"],
-                path: "Modules/IPAddressProvider/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "CatalogServer", targets: ["CatalogServer"]),
-        [
-            .target(
-                name: "CatalogServer",
-                dependencies: [
-                    "Swinject",
-                    "ToolKit",
-                    "Models",
-                    "Contracts",
-                    "IPAddressProvider",
-                    .product(name: "GRPC", package: "grpc-swift")
-                ],
-                path: "Modules/CatalogServer/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "CatalogClient", targets: ["CatalogClient"]),
-        [
-            .target(
-                name: "CatalogClient",
-                dependencies: [
-                    "Swinject",
-                    "ToolKit",
-                    "Models",
-                    "Contracts",
-                ],
-                path: "Modules/CatalogClient/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "CatalogSource", targets: ["CatalogSource"]),
-        [
-            .target(
-                name: "CatalogSource",
-                dependencies: [
-                    "Swinject",
-                    "ToolKit",
-                    "Models",
-                    "Contracts",
-                    "Database",
-                    "CatalogClient",
-                    "SharedInterfaces",
-                    .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
-                ],
-                path: "Modules/CatalogSource/Sources"
-            ),
-        ]
-    ),
-    (
-        .library(name: "Logger", targets: ["Logger"]),
-        [
-            .target(
-                name: "Logger",
-                dependencies: [
-                    "ToolKit",
-                    "Swinject",
-                    .product(name: "Logging", package: "swift-log"),
-                ],
-                path: "Modules/Logger/Sources"
-            ),
-        ]
-    ),
+// MARK: - Public
 
-    // MARK: - Features
+enum Module: String, CaseIterable, Equatable {
+    case CatalogClient
+    case CatalogServer
+    case CatalogSource
+    case Contracts
+    case Database
+    case IPAddressProvider
+    case Logger
+    case Models
+    case SharedInterfaces
+    case ToolKit
+    // Features
+    case AddItemFeature
+    case CatalogFeature
+    case RemoteFeature
+    case RootFeature
+}
 
-    (
-        .library(name: "RootFeature", targets: ["RootFeature"]),
-        [
-            .target(
-                name: "RootFeature",
-                dependencies: [
-                    "ToolKit",
-                    "SharedInterfaces",
-                    "Models",
-                    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-                    .product(name: "Swinject", package: "Swinject")
-                ],
-                path: "Modules/Features/RootFeature/Sources"
-            ),
-        ]
+enum Dependency {
+    case module(Module)
+    case external(Target.Dependency)
+}
+
+/// Modules has common dependency list, see below
+let dependencyMap: [Module: [Dependency]] = [
+    .SharedInterfaces: [
+        .module(.Models),
+        .external(.product(name: "IdentifiedCollections", package: "swift-identified-collections")),
+    ],
+    .CatalogClient: [
+        .module(.Models),
+        .module(.Contracts),
+    ],
+    .CatalogServer: [
+        .module(.Models),
+        .module(.Contracts),
+        .module(.IPAddressProvider),
+        .external(.product(name: "GRPC", package: "grpc-swift")),
+    ],
+    .CatalogSource: [
+        .module(.Models),
+        .module(.Contracts),
+        .module(.Database),
+        .module(.CatalogClient),
+        .module(.SharedInterfaces),
+        .external(.product(name: "IdentifiedCollections", package: "swift-identified-collections")),
+    ],
+    .Contracts: [
+        .external(.product(name: "GRPC", package: "grpc-swift")),
+    ],
+    .Logger: [
+        .external(.product(name: "Logging", package: "swift-log")),
+    ],
+    // Features
+    .AddItemFeature: [
+        .module(.CatalogSource),
+    ],
+    .RemoteFeature: [
+        .module(.CatalogServer),
+    ],
+]
+
+let commonModuleDependencies: [Dependency] = [
+    .module(.Logger),
+    .module(.ToolKit),
+    .external("Swinject"),
+]
+
+let commonFeatureDependencies: [Dependency] = [
+    .module(.SharedInterfaces),
+    .module(.Models),
+    .external(.product(name: "ComposableArchitecture", package: "swift-composable-architecture")),
+    .external(.product(name: "IdentifiedCollections", package: "swift-identified-collections")),
+]
+
+let externalDependecies: [Package.Dependency] = [
+    .package(
+        name: "swift-composable-architecture",
+        url: "https://github.com/pointfreeco/swift-composable-architecture",
+        branch: "main"
     ),
-    (
-        .library(name: "CatalogFeature", targets: ["CatalogFeature"]),
-        [
-            .target(
-                name: "CatalogFeature",
-                dependencies: [
-                    "ToolKit",
-                    "Contracts",
-                    "SharedInterfaces",
-                    "Database",
-                    "Models",
-                    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-                    .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
-                    .product(name: "Swinject", package: "Swinject"),
-                ],
-                path: "Modules/Features/CatalogFeature/Sources"
-            ),
-        ]
+    .package(
+        url: "https://github.com/grpc/grpc-swift.git",
+        from: "1.0.0"
     ),
-    (
-        .library(name: "AddItemFeature", targets: ["AddItemFeature"]),
-        [
-            .target(
-                name: "AddItemFeature",
-                dependencies: [
-                    "ToolKit",
-                    "SharedInterfaces",
-                    "Models",
-                    "CatalogSource",
-                    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-                    .product(name: "Swinject", package: "Swinject"),
-                ],
-                path: "Modules/Features/AddItemFeature/Sources"
-            ),
-        ]
+    .package(
+        name: "Swinject",
+        url: "https://github.com/Swinject/Swinject",
+        from: "2.8.1"
     ),
-    (
-        .library(name: "RemoteFeature", targets: ["RemoteFeature"]),
-        [
-            .target(
-                name: "RemoteFeature",
-                dependencies: [
-                    "ToolKit",
-                    "SharedInterfaces",
-                    "Models",
-                    "CatalogServer",
-                    .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-                    .product(name: "Swinject", package: "Swinject"),
-                ],
-                path: "Modules/Features/RemoteFeature/Sources"
-            )
-        ]
+    .package(
+        name: "swift-identified-collections",
+        url: "https://github.com/pointfreeco/swift-identified-collections",
+        from: "0.3.2"
+    ),
+    .package(
+        url: "https://github.com/apple/swift-log.git",
+        from: "1.0.0"
     ),
 ]
+
+// MARK: - Private
+
+let packageContent: [(Product, [Target])] = {
+    var modules = Module.allCases.reduce(into: [(Product, [Target])]()) { array, module in
+        let moduleName = module.rawValue
+        let isFeature = moduleName.hasSuffix("Feature")
+        var targetDependencies = [Target.Dependency]()
+        var dependenciesToConvert = [Dependency]()
+
+        if !commonModuleDependencies.contains(where: {
+            if case let .module(currentModule) = $0 {
+                return currentModule == module
+            } else {
+                return false
+            }
+        }) {
+            dependenciesToConvert.append(contentsOf: commonModuleDependencies)
+        }
+
+        if isFeature {
+            dependenciesToConvert.append(contentsOf: commonFeatureDependencies)
+        }
+
+        if let definedDependencies = dependencyMap[module] {
+            dependenciesToConvert.append(contentsOf: definedDependencies)
+        }
+
+        dependenciesToConvert.forEach {
+            switch $0 {
+            case let .external(dependency):
+                targetDependencies.append(dependency)
+            case let .module(depModule):
+                if depModule != module {
+                    targetDependencies.append(.init(stringLiteral: depModule.rawValue))
+                }
+            }
+        }
+
+        let product = Product.library(name: moduleName, targets: [moduleName])
+
+        let target = Target.target(
+            name: moduleName,
+            dependencies: targetDependencies,
+            path: "Modules\(isFeature ? "/Features" : "")/\(moduleName)/Sources"
+        )
+
+        array.append((product, [target]))
+    }
+
+    // Assembler module
+
+    var assemblerDependencies: [Target.Dependency] = Module.allCases.map {
+        .init(stringLiteral: $0.rawValue)
+    }
+
+    assemblerDependencies.append(.init(stringLiteral: "Swinject"))
+
+    modules.append(
+        (
+            .library(name: "AppAssembler", targets: ["AppAssembler"]),
+            [
+                .target(
+                    name: "AppAssembler",
+                    dependencies: assemblerDependencies,
+                    path: "Modules/AppAssembler/Sources"
+                )
+            ]
+        )
+    )
+
+    return modules
+}()
 
 let package = Package(
     name: "CopyPastaModules",
     platforms: [ .iOS(.v15) ],
-    products: content.map { $0.0 },
-    dependencies: [
-        .package(
-            name: "swift-composable-architecture",
-            url: "https://github.com/pointfreeco/swift-composable-architecture",
-            branch: "main"
-        ),
-        .package(
-            url: "https://github.com/grpc/grpc-swift.git",
-            from: "1.0.0"
-        ),
-        .package(
-            name: "Swinject",
-            url: "https://github.com/Swinject/Swinject",
-            from: "2.8.1"
-        ),
-        .package(
-            name: "swift-identified-collections",
-            url: "https://github.com/pointfreeco/swift-identified-collections",
-            from: "0.3.2"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-log.git",
-            from: "1.0.0"
-        ),
-    ],
-    targets: content.flatMap { $0.1 }
+    products: packageContent.map { $0.0 },
+    dependencies: externalDependecies,
+    targets: packageContent.flatMap { $0.1 }
 )
