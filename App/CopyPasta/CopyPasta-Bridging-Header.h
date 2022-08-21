5ac96962170cc78e30ea91b97254c782cf6ef96b
Mikhail Kuzmin <mkuzmin231@gmail.com>
2022-08-21 19:10:55 +0300

    Rename project

--- App/CopyPasta/CopyPasta-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-#ifndef CopyPasta_Bridging_Header_h
-#define CopyPasta_Bridging_Header_h
-
-#include<ifaddrs.h>
-
-#endif /* CopyPasta_Bridging_Header_h */
---
+++ App/Links/Links-Bridging-Header.h
@@ -0,0 +1,6 @@
+#ifndef Links_Bridging_Header_h
+#define Links_Bridging_Header_h
+
+#include<ifaddrs.h>
+
+#endif /* Links_Bridging_Header_h */
